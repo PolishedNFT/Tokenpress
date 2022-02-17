0ee520c1af7710df7d3fe49a6374a92404a98b95
schwarzjake <schwarz.jake@protonmail.com>
2022-02-16 18:09:36 -0700

    add commandline argument

--- src/main.js
+++ src/main.js
@@ -5,34 +5,50 @@ const { performance } = require('perf_hooks');
 const settings = {
 	name: '',
 	symbol: '',
-	total: 1,
-	max_per: 1,
-	price: 25000000000000000, // 0.025 Ether
+	max_per: 10,
+	price: 20000000000000000, // 0.020000000000000000 ether (18 places)
 };
 
-const buildPath = path.resolve(__dirname, '../build');
-
-function printingPress(templateName, replacements) {
-	const tmp = String(fs.readFileSync(path.resolve(__dirname, `../template/${templateName}`)));
-
-	const data = tmp.replace(/\$\{([^\}]+)\}/g, (matched, target) => replacements[target]);
-
-	if (!fs.existsSync(buildPath)) {
-		fs.mkdirSync(buildPath);
+async function getManifest() {
+	if (process.argv.length !== 3) {
+		console.log('[?] Usage: npm run start -- ../path/to/input/');
+		throw new Error('Invalid amount of arguments passed.');
 	}
 
-	fs.writeFileSync(`${buildPath}/${replacements.name}.sol`, data);
-
-	console.log(`[+] Printed ${replacements.name} [${replacements.symbol}] ${replacements.total}`);
+	let arg = process.argv[2];
+	if (arg.length > 1 && arg[arg.length - 1] === '/') {
+		arg = arg.slice(0, arg.length - 1);
+	}
+	const content = await fs.promises.readFile(`${arg}/manifest.json`);
+	return JSON.parse(content);
 }
 
-function main() {
+async function main() {
 	console.log(`TokenPress [v1.0.0]`);
 	console.log('---------------------------------');
 
 	const startTime = performance.now();
 
-	printingPress('ERC721.sol', settings);
+	const manifest = await getManifest();
+
+	const replacements = {
+		...settings,
+		total: manifest.total,
+		provenanceHash: manifest.provenanceHash,
+		baseUri: manifest.baseUri,
+	};
+
+	const buildDir = path.resolve(__dirname, '../build');
+
+	if (!fs.existsSync(buildDir)) {
+		fs.mkdirSync(buildDir);
+	}
+
+	const tmp = String(fs.readFileSync(path.resolve(__dirname, `../template/ERC721.sol`)));
+	const data = tmp.replace(/\$\{([^\}]+)\}/g, (matched, target) => replacements[target]);
+	fs.writeFileSync(`${buildDir}/${replacements.name}.sol`, data);
+
+	console.log(`[+] Printed ${replacements.name} [${replacements.symbol}] ${replacements.total}`);
 
 	const endTime = (Math.abs(performance.now() - startTime) / 1000).toFixed(4);
 
@@ -40,4 +56,7 @@ function main() {
 	console.log(`Printing completed in ${endTime}s`);
 }
 
-main();
+main().catch(err => {
+	console.error(err);
+	process.exit(1);
+});
